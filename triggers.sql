a71fc5f513d7a7294937398bd16edb629189fc84
Viiuska <87257685+Viiuska@users.noreply.github.com>
2023-05-04 14:12:33 +0300

    done

--- triggers.sql
+++ triggers.sql
@@ -1,4 +1,4 @@
---Triggers
+--Triggers first and last
 
 CREATE OR REPLACE FUNCTION check_skills()
 RETURNS TRIGGER AS $$
@@ -15,6 +15,33 @@ BEFORE INSERT ON skills
 FOR EACH ROW
 EXECUTE FUNCTION check_skills();
 
+--Testing the trigger
+INSERT INTO skills (s_id, skill, salary_benefit, salary_benefit_value)
+VALUES (37, 'SQL', 'false', 0);
 
 
+CREATE OR REPLACE FUNCTION update_contract()
+RETURNS TRIGGER AS $$
+BEGIN
+	IF NEW.contract_type = 'Temporary' THEN
+		NEW.contract_start = NOW();
+		NEW.contract_end = NEW.contract_end+ INTERVAL '2 years';
+	ELSE 
+		NEW.contract_start = NOW();
+		NEW.contract_end = NULL;
+	END IF;
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
 
+CREATE TRIGGER update_contarct_trigger
+BEFORE UPDATE ON employee
+FOR EACH ROW
+WHEN (OLD.contract_type != NEW.contract_type)
+EXECUTE FUNCTION update_contract();
+
+
+--Testing the trigger
+UPDATE employee
+SET contract_type = 'Part-time'
+WHERE e_id = 2;
